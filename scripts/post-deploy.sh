db2e65f726cfbaa529f1145e6366ad088e310ebe
GaryChang <garychang1214@gmail.com>
2024-09-01 23:39:50 -0400

    test(deploy) : test aws code-deploy

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+set -e
 
+LOGFILE=/home/ec2-user/deploy.log
+
+echo "Starting post-deploy script..." | tee -a $LOGFILE
 
 cd /home/ec2-user/Teaching-Platform/client
 
@@ -14,3 +18,5 @@ sudo pm2 restart all
 
 
 sudo systemctl restart nginx
+
+echo "Post-deploy script completed successfully." | tee -a $LOGFILE
