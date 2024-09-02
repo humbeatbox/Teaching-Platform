17e4b5ba224c38e857e93f59d51efeb7a4bf33eb
GaryChang <garychang1214@gmail.com>
2024-09-02 01:48:44 -0400

    fix(deploy) : fix appspec.yml and post shell

--- scripts/post-deploy.sh
+++ scripts/post-deploy.sh
@@ -5,18 +5,20 @@ LOGFILE=/home/ec2-user/deploy.log
 
 echo "Starting post-deploy script..." | tee -a $LOGFILE
 
+echo "Changing directory to /home/ec2-user/Teaching-Platform/client..." | tee -a $LOGFILE
 cd /home/ec2-user/Teaching-Platform/client
 
-
+echo "Running npm run build..." | tee -a $LOGFILE
 sudo npm run build
 
+echo "Moving build files to ../server/client/build..." | tee -a $LOGFILE
+sudo rm -rf ../server/client/build
+sudo mv build/ ../server/client/
 
-sudo mv -f build/ ../server/client/build
-
-
-sudo pm2 restart all
-
-
+echo "Restarting PM2 processes..." | tee -a $LOGFILE
+#pm2 restart backend --update-env
+pm2 reload all
+echo "Restarting Nginx..." | tee -a $LOGFILE
 sudo systemctl restart nginx
 
 echo "Post-deploy script completed successfully." | tee -a $LOGFILE
